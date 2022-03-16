f678b5dc4055f5366bc5b0a9137333b4f5646723
Joseph Maramba <josephmaramba50@gmail.com>
2022-03-16 04:50:45 +0300

    http

--- src/index.test.js
+++ src/index.test.js
@@ -2,20 +2,19 @@ import { expect } from "chai";
 import jsdom from "jsdom";
 import fs from "fs";
 
-describe("first test", () => {
+describe("Our first test", () => {
   it("should pass", () => {
     expect(true).to.equal(true);
   });
 });
 
 describe("index.html", () => {
-  it("should say hello", () => {
+  it("should have h1 that says users", () => {
     const index = fs.readFileSync("./src/index.html", "utf-8");
     const { JSDOM } = jsdom;
     const dom = new JSDOM(index);
     const h1 = dom.window.document.getElementsByTagName("h1")[0];
-
-    expect(h1.innerHTML).to.equal("Hello world!");
+    expect(h1.innerHTML).to.equal("Users");
     dom.window.close();
   });
 });
